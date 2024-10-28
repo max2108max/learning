6539d818a8502ea28751f292d30867c6e69fc901
max2108max <max21bold@gmail.com>
2024-10-29 01:17:57 +1000

    задачи на var

--- modules/Learning/testing.js
+++ modules/Learning/testing.js
@@ -1,14 +1,5 @@
+var a = 5;
 
-function get(obj, path){
-
-}
-const obj = {
-    a:{
-        b:{
-            c:"d"
-        },
-        e:"f"
-    }
-};
-get(obj,"a.b")//c:d
-get(obj,"a.b.x")// undefined
+(function() {
+  alert(a)
+})()
